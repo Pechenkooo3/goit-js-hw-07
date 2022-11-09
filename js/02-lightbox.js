ca89c07b4efa480706a6ba52dd9d3242e5033325
Valov Ivan <pechenkooo3@gmail.com>
2022-11-09 19:14:33 +0200

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+function createMarkUp(galleryItems) {
+  return galleryItems
+    .map((item) => {
+      return `
+      <a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+     `;
+    })
+    .join("");
+}
+
+const imgGalleryEl = document.querySelector(".gallery");
+const markupGallery = createMarkUp(galleryItems);
+imgGalleryEl.innerHTML = markupGallery;
+imgGalleryEl.addEventListener("click", onClickImg);
+
+let instance = "";
+function onClickImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+  document.addEventListener("keydown", onModalCloseToEscape);
+}
+function onModalCloseToEscape(evt) {
+  if (evt.code === "Escape") {
+    instance.close();
+    document.removeEventListener("keydown", onModalCloseToEscape);
+  }
+}
+
